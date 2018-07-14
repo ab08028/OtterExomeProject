b347c519d6fd40c4a987d29c32c1c6bbfabb32cf
Annabel Beichman <annabel.beichman@gmail.com>
2018-07-14 09:54:33 -0700

    ** testing aDNA mapping to multiple genomes **

--- scripts/scripts_20180521/data_processing/paleomixPipeline/plmx_Step_3_samtoolsFlagstat.sh
+++ scripts/scripts_20180521/data_processing/paleomixPipeline/plmx_Step_3_samtoolsFlagstat.sh
@@ -2,7 +2,6 @@ module load samtools
 # file locations:
 SCRATCH=/u/flashscratch/a/ab08028
 wd=$SCRATCH/captures/paleomix/ancient_FirstRun_noIndelR_20180712
-makefileDir=$scriptDir/makefiles/ancientMakefiles
 headers=$SCRATCH/captures/samples/ancientSamples.txt
 REFPREFIX=Mustela_putorius_furo.MusPutFur1.0.dna.toplevel
 
--- scripts/scripts_20180521/data_processing/paleomixPipeline/testingADNAMapping/plmx_Step_0_b_MakeAllMakefiles.TestMapping.ancient.sh
+++ scripts/scripts_20180521/data_processing/paleomixPipeline/testingADNAMapping/plmx_Step_0_b_MakeAllMakefiles.TestMapping.ancient.sh
@@ -11,12 +11,12 @@ SCRATCH=/u/flashscratch/a/ab08028
 wd=$SCRATCH/captures
 makefileDir=$scriptDir/paleomixPipeline/makefiles
 modernTemplate=$makefileDir/makefile_template.modernDNA.yaml
-ancientTemplate=$makefileDir/makefile_template.aDNA.yaml
+ancientTemplate=$makefileDir/makefile_template.aDNA.noIndelR.testMultGenomes.yaml
 ancHeaders=$wd/samples/ancientSamples.txt
-modHeaders=$wd/samples/modernSamples.txt
+#modHeaders=$wd/samples/modernSamples.txt
 
-mkdir -p $makefileDir/ancientMakefiles
-mkdir -p $makefileDir/modernMakefiles
+mkdir -p $makefileDir/ancientMakefiles-TestMapping
+#mkdir -p $makefileDir/modernMakefiles
 ########### ancient makefiles ########
 # instead of START/END, using while read list of samples
 cat $ancHeaders | while read header
@@ -26,8 +26,8 @@ echo $header
 # make a new version of the makefile
 ######## *** ALWAYS MAKE SURE SETTINGS ARE APPROPRIATE *** ###########
 # calling /bin/cp because my cp is aliased to be interactive
-/bin/cp $ancientTemplate $makefileDir/ancientMakefiles/${header}.paleomix.makefile.yaml
-newMake=$makefileDir/ancientMakefiles/${header}.paleomix.makefile.yaml
+/bin/cp $ancientTemplate $makefileDir/ancientMakefiles-TestMapping/${header}.paleomix.makefile.yaml
+newMake=$makefileDir/ancientMakefiles-TestMapping/${header}.paleomix.makefile.yaml
 # for now NAME OF TARGET and SAMPLE are going to be the same
 sed -i'' "s/NAME_OF_TARGET:/$header:/g" $newMake
 sed -i'' "s/NAME_OF_SAMPLE:/$header:/g" $newMake
@@ -38,28 +38,5 @@ sed -i'' "s/NAME_OF_LANE:/Lane_1:/g" $newMake
 sed -i'' 's|: PATH_WITH_WILDCARDS|: '${fastqs}\/${header}_S*_R{Pair}_*fastq.gz'|g' $newMake
 
 # clear variables
-newMake=''
-done
-
-########### modern makefiles ########
-# modern dna
-cat $modHeaders | while read header
-do
-echo $header
-# note the need for double quotation marks for sed
-# make a new version of the makefile
-/bin/cp $modernTemplate $makefileDir/modernMakefiles/${header}.paleomix.makefile.yaml
-newMake=$makefileDir/modernMakefiles/${header}.paleomix.makefile.yaml
-# for now NAME OF TARGET and SAMPLE are going to be the same
-sed -i'' "s/NAME_OF_TARGET:/$header:/g" $newMake
-sed -i'' "s/NAME_OF_SAMPLE:/$header:/g" $newMake
-sed -i'' "s/NAME_OF_LIBRARY:/${header}_1a:/g" $newMake
-# for now just naming Lane: Lane 1 because just one lane of novaseq
-sed -i'' "s/NAME_OF_LANE:/Lane_1:/g" $newMake
-# use different delims (|) to avoid filepath slash confusion:
-sed -i'' 's|: PATH_WITH_WILDCARDS|: '${fastqs}\/${header}_S*_R{Pair}_*fastq.gz'|g' $newMake
-# need to say "{Pair}" to have it find both copies. See if this works.
-# clear variables
-# note that if sed is run on an empty file, it creates randomly named weird empty file. Not a big deal. e.g. sedVeLdud
 newMake=''
 done
