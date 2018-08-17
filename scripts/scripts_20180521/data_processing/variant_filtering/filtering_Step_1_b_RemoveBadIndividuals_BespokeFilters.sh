ecd4bed64e1e100cb9e365dcfda7420c3506222f
Annabel Beichman <ab08028@login1.(none)>
2018-08-16 17:53:23 -0700

    updated bad ind

--- scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_1_b_RemoveBadIndividuals_BespokeFilters.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_1_b_RemoveBadIndividuals_BespokeFilters.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #$ -cwd
-#$ -l h_rt=50:00:00,h_data=16G,highp,arch=intel*
+#$ -l h_rt=100:00:00,h_data=16G,highp,arch=intel*
 #$ -N vcf1b_filtering
 #$ -o /u/flashscratch/a/ab08028/captures/reports/GATK
 #$ -e /u/flashscratch/a/ab08028/captures/reports/GATK
