1b1a920175765be4f77c36371093e83563594fd1
Annabel Beichman <annabel.beichman@gmail.com>
2018-08-20 10:18:01 -0700

    separate populations without for loop

--- scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_1_d_separateByPopulation.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_1_d_separateByPopulation.sh
@@ -28,6 +28,7 @@ outdir=$wd/${rundate}_filtered # date you called genotypes
 # want to separate vcfs by population
 mkdir -p $outdir/populationVCFs
 populations="CA AL KUR AK"
+
 # treat commanders specially
 
 # need to combine Bering and Medny separately (and label as COM (commanders))
@@ -35,28 +36,51 @@ populations="CA AL KUR AK"
 # note: I'm making it so that every individual in the population has to be called at the site,
 # but across all the populations only 80% have to be called at the site. I think this makes sense
 
-# want to do it for combined nv/snp files:
+# Bering/Medny --> Commanders (COM)
+#java -jar $GATK \
+#-R $REFERENCE \
+#-T SelectVariants \
+#--variant ${outdir}/'all_7_passingBespoke_maxNoCallFrac_'${noCallFrac}'_rmBadIndividuals_passingFilters_'${infile} \
+#-o ${outdir}/populationVCFs/COM_'all_7_passingAllFilters_allCalled'${infile} \
+#-se '.+_Elut_BER_.+' \
+#-se '.+_Elut_MED_.+' \
+#--maxNOCALLfraction 0
+# worked
+# California --> CA
 java -jar $GATK \
 -R $REFERENCE \
 -T SelectVariants \
 --variant ${outdir}/'all_7_passingBespoke_maxNoCallFrac_'${noCallFrac}'_rmBadIndividuals_passingFilters_'${infile} \
--o ${outdir}/populationVCFs/COM_'all_7_passingAllFilters_allCalled'${infile} \
--se '.+_Elut_BER_.+' \
--se '.+_Elut_MED_.+' \
+-o ${outdir}/populationVCFs/CA_'all_7_passingAllFilters_allCalled'${infile} \
+-se '.+_Elut_CA_.+' \
 --maxNOCALLfraction 0
 
-for pop in $populations
-do
-echo $pop
-
+# Alaska --> AK
 java -jar $GATK \
 -R $REFERENCE \
 -T SelectVariants \
 --variant ${outdir}/'all_7_passingBespoke_maxNoCallFrac_'${noCallFrac}'_rmBadIndividuals_passingFilters_'${infile} \
--o ${outdir}/populationVCFs/${pop}_'all_7_passingAllFilters_allCalled'${infile} \
--se '.+_Elut_${pop}_.+' \
+-o ${outdir}/populationVCFs/AK_'all_7_passingAllFilters_allCalled'${infile} \
+-se '.+_Elut_AK_.+' \
 --maxNOCALLfraction 0
 
-done
+# Aleutian --> AL
+java -jar $GATK \
+-R $REFERENCE \
+-T SelectVariants \
+--variant ${outdir}/'all_7_passingBespoke_maxNoCallFrac_'${noCallFrac}'_rmBadIndividuals_passingFilters_'${infile} \
+-o ${outdir}/populationVCFs/AL_'all_7_passingAllFilters_allCalled'${infile} \
+-se '.+_Elut_AL_.+' \
+--maxNOCALLfraction 0
 
 
+# Kuril --> KUR
+java -jar $GATK \
+-R $REFERENCE \
+-T SelectVariants \
+--variant ${outdir}/'all_7_passingBespoke_maxNoCallFrac_'${noCallFrac}'_rmBadIndividuals_passingFilters_'${infile} \
+-o ${outdir}/populationVCFs/KUR_'all_7_passingAllFilters_allCalled'${infile} \
+-se '.+_Elut_KUR_.+' \
+--maxNOCALLfraction 0
+
+# using $pop inside regex doesn't work; going pop by pop 
