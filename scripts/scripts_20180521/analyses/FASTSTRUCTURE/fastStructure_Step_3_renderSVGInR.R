d5d7f2fe74b606dbee6a0340c721aa546ff70545
Annabel Beichman <annabel.beichman@gmail.com>
2018-08-27 13:50:32 -0700

    updated pca and relatedness scripts

--- scripts/scripts_20180521/analyses/FASTSTRUCTURE/fastStructure_Step_3_renderSVGInR.R
+++ scripts/scripts_20180521/analyses/FASTSTRUCTURE/fastStructure_Step_3_renderSVGInR.R
@@ -1,7 +1,9 @@
 ##### Faststructure outputs SVG plots 
 # download them and copy to : /Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/plots/FASTSTRUCTURE
-wd="/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/analysisResults/FASTSTRUCTURE/"
 calldate=20180806 # date genotypes were called
+wd=paste("/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/analysisResults/FASTSTRUCTURE/",calldate,"/",sep="")
+outdir=paste("/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/plots/FASTSTRUCTURE/",calldate,"/",sep="")
+dir.create(outdir)
 # install.packages("rsvg")
 # install.packages("svglite")
 require(rsvg)
@@ -9,6 +11,6 @@ require(rsvg)
 # convert to PDF
 for (i in seq(1,10)){
   print(i)
-  svg1File <- paste(wd,calldate,"/plots/snp_5_passingAllFilters_postMerge_raw_variants.faststructure_plot.",i,".svg",sep="")
-  rsvg_pdf(svg1File, paste(wd,calldate,"/plots/snp_5_passingAllFilters_postMerge_raw_variants.faststructure_plot",i,".pdf",sep=""))
+  svg1File <- paste(wd,"/svg/snp_7_downSampCOM_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants.faststructure_plot.",i,".svg",sep="")
+  rsvg_pdf(svg1File, paste(outdir,"/snp_7_downSampCOM_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants.faststructure_plot.",i,".pdf",sep=""))
 }
