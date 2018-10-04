a646b2ee67e268e3f30180460aa67bdcb980b5af
Annabel Beichman <ab08028@n9987.(none)>
2018-10-04 13:41:16 -0700

    fixed path in step 1c

--- scripts/scripts_20180521/data_processing/initialSteps_setupQC/Step_0_c_FastQC.Hoffman.submit.sh
+++ scripts/scripts_20180521/data_processing/initialSteps_setupQC/Step_0_c_FastQC.Hoffman.submit.sh
@@ -13,14 +13,16 @@ QSUB=/u/systems/UGE8.0.1vm/bin/lx-amd64/qsub
 # location of github:  which may be on remote server or local laptop
 gitDir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
 # scripts:
-scriptDir=$gitDir/scripts/scripts_20180521/data_processing/variant_calling
+scriptDir=$gitDir/scripts/scripts_20180521/data_processing/
 # script to run: 
 scriptname=Step_0_c_FastQC.Hoffman.sh # change this to final script name!! 
 # 
 # file locations:
 SCRATCH=/u/flashscratch/a/ab08028
 wd=$SCRATCH/captures
-headers=$wd/samples/allElutSamples.txt # all Elut samples, modern, ancient and blank
+#headers=$wd/samples/allElutSamples.txt # all Elut samples, modern, ancient and blank
+# 20181004: screening libraries
+headers=$wd/samples/ancientLibs.Screen.txt
 errorLocation=/u/flashscratch/a/ab08028/captures/reports/fastqc/
 mkdir -p $errorLocation
 
