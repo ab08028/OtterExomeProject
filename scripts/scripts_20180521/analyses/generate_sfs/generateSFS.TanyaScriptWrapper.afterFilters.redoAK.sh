36ade116cd00e30dfb6d1aec5dacf2c1a1d9baa8
Annabel Beichman <ab08028@n2001.(none)>
2018-10-09 12:53:10 -0700

    updated sfs script to use neutral bed file that is based on max no call frac of 0.9 across populations (still requires 100% call for each populaiton, but expands available neutral regions by 750kb)

--- scripts/scripts_20180521/analyses/generate_sfs/generateSFS.TanyaScriptWrapper.afterFilters.redoAK.sh
+++ scripts/scripts_20180521/analyses/generate_sfs/generateSFS.TanyaScriptWrapper.afterFilters.redoAK.sh
@@ -13,23 +13,15 @@ source /u/local/Modules/default/init/modules.sh
 module load python/2.7
 
 rundate=20180806
-todaysdate=`date +%Y%m%d` # want to add date to sfs output
 ### wrapper for Tanya's script
 SCRATCH=/u/flashscratch/a/ab08028
 
 # location of per-population input VCFs (with NO no-call genotypes)
 vcfdir=$SCRATCH/captures/vcf_filtering/${rundate}_filtered
 
-# this is the old neutral bed file from old filtering scheme results that used 0.2 max no call frac across all pops which is too strict (these regions are good regions, there just might be more out there with new filtering; this is a conservative set)
-#neutralBed=${vcfdir}/oldFilteringSchemeResults/bedCoords/all_7_passingBespoke.min10kb.fromExon.noCpGIsland.noRepeat.noFish.0based.sorted.merged.useThis.bed
-# 20181009 this is the new neutral sites bed file -->
-# it has been filtered for:
-# is 10kb from exons
-# is not in CpG island
-# is not in repeat region
-# does not blast to zebra fish
-# no other GC content filter
-neutralBed=${vcfdir}/bedCoords/all_8_rmRelatives_keepAdmixed_passingBespoke_maxNoCallFrac_0.9_rmBadIndividuals_passingFilters.min10kb.fromExon.noCpGIsland.noRepeat.noFish.0based.sorted.merged.bed
+### ** TEMPORARY *** note: for now, using neutral bed from old filtering scheme results -- eventually re-do (these regions are good regions, there just might be more out there with new filtering; this is a conservative set)
+neutralBed=${vcfdir}/oldFilteringSchemeResults/bedCoords/all_7_passingBespoke.min10kb.fromExon.noCpGIsland.noRepeat.noFish.0based.sorted.merged.useThis.bed
+
 # output SFS location
 SFSdir=$SCRATCH/captures/analyses/SFS/${rundate}
 mkdir -p $SFSdir/neutralSFS
@@ -41,8 +33,8 @@ mkdir -p $SFSdir/neutralSFS/admixed
 tanyaDir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/scripts/scripts_20180521/scripts_from_others/tanya_scripts/
 
 # generate folded SFS:
-populations="CA AK AL COM KUR"
-
+#populations="CA AK AL COM KUR"
+populations="AK"
 for pop in $populations
 do
 echo $pop
@@ -52,20 +44,21 @@ inVCF=${pop}_'all_9_rmAllHet_rmRelativesAdmixed_passingAllFilters_allCalled.vcf.
 
 python $tanyaDir/popgen_tools/popgen_tools.py \
 --vcf_file $vcfdir/populationVCFs/$inVCF \
---sfs_out $SFSdir/neutralSFS/${inVCF%.vcf.gz}.filtered.sfs.${todaysdate}.out \
+--sfs_out $SFSdir/neutralSFS/${inVCF%.vcf.gz}.filtered.sfs.out \
 --no_pi \
 --target_bed $neutralBed
+
 
 done
 
 # admixed SFSes:
-for pop in KUR AK
+for pop in AK
 do
 echo "admixed " $pop
 inVCF='admixIndOnly_'${pop}'_all_9_rmAllHet_passingAllFilters_allCalled.vcf.gz'
 python $tanyaDir/popgen_tools/popgen_tools.py \
 --vcf_file $vcfdir/populationVCFs/admixedVCFs/$inVCF \
---sfs_out $SFSdir/neutralSFS/admixed/${inVCF%.vcf.gz}.filtered.sfs.${todaysdate}.out \
+--sfs_out $SFSdir/neutralSFS/admixed/${inVCF%.vcf.gz}.filtered.sfs.out \
 --no_pi \
 --target_bed $neutralBed
 done
