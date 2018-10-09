a0a6ed7bca31f974bb2514a571cde34fad9b241e
Annabel Beichman <annabel.beichman@gmail.com>
2018-10-09 15:54:04 -0700

    made changes to calculations of pi per population; made it so that it will only calculate it in neutral regions that are defined by the actually called sites for a population; not the overall neutral regions for which a pop may not be called at a particular site

--- scripts/scripts_20180521/analyses/calculate_pi/calculateNeutralPi.TanyaWrapper.sh
+++ scripts/scripts_20180521/analyses/calculate_pi/calculateNeutralPi.TanyaWrapper.sh
@@ -18,8 +18,11 @@ SCRATCH=/u/flashscratch/a/ab08028
 
 # location of per-population input VCFs (with NO no-call genotypes)
 vcfdir=$SCRATCH/captures/vcf_filtering/${rundate}_filtered
-neutralBed=${vcfdir}/bedCoords/all_7_passingBespoke.min10kb.fromExon.noCpGIsland.noRepeat.noFish.0based.sorted.merged.useThis.bed
 
+# temporary old neutral bed (probably can get more regions under new filtering scheme; this is a good conservative set)
+
+neutralBed=${vcfdir}/bedCoords/all_8_rmRelatives_keepAdmixed_passingBespoke_maxNoCallFrac_0.9_rmBadIndividuals_passingFilters.min10kb.fromExon.noCpGIsland.noRepeat.noFish.0based.sorted.merged.useThis.bed
+mkdir -p ${vcfdir}/bedCoords/neutralCallableSites_perPop
 # output SFS location
 pidir=$SCRATCH/captures/analyses/pi/${rundate}
 mkdir -p $pidir/neutralPi
@@ -37,14 +40,24 @@ do
 echo $pop
 
 # if vcf file is not prefiltered to just contain neutral (or other) regions
-inVCF=${pop}_all_7_passingAllFilters_allCalledraw_variants.vcf.gz
-
+inVCF=${pop}_all_9_rmAllHet_rmRelativesAdmixed_passingAllFilters_allCalled.vcf.gz
+# want to get callable sites in neutral regions per population; want a to be the vcf file because want to write out 
+# this will find every site that is in the bed neutral regions that is present in the VCF
+# it will output them as single sites in the bed:
+# like this: GL896898.1	752539	752540
+# and you can then sort and merge that bed to get new neutral regions for the specific pop
+bedtools intersect -a $neutralBed -b $vcfdir/populationVCFs/$inVCF | bedtools sort -i stdin | bedtools merge -i stdin > ${vcfdir}/bedCoords/neutralCallableSites_perPop/${inVCF%.vcf.gz}.neutral.callableSites.0based.bed
+popNeutBed=${vcfdir}/bedCoords/neutralCallableSites_perPop/${inVCF%.vcf.gz}.neutral.callableSites.0based.bed
 python $tanyaDir/popgen_tools/popgen_tools.py \
 --vcf_file $vcfdir/populationVCFs/$inVCF \
---pi_out $pidir/neutralPi/${inVCF%.vcf.gz}.pi.out\
---total_SNPs $pidir/neutralPi/${inVCF%.vcf.gz}.totalSNPs.out \
+--pi_out $pidir/neutralPi/${inVCF%.vcf.gz}.pi.perPopCallableSiteNeutralRegions.out \
+--total_SNPs $pidir/neutralPi/${inVCF%.vcf.gz}.totalSNPs.perPopCallableSiteNeutralRegions.out \
 --no_sfs \
---target_bed $neutralBed
-
-
+--target_bed $popNeutBed # instead of overall all-pop neutral regions, some of which may not be called int his population, use the actual ALL-CALLED neutral regions. will make pi calculations easier
 done
+
+# Note that now the output will be 
+# start_region stop_region pi_in_region (not pi per site)
+# note that if you use a general bed of neutral regions then this output won't represent callable sites
+# but in this new way I'm doing it where I get the callable sites specifically beforehand, then it actually will
+# give you the callable neutral sites that have passed all filters! 
