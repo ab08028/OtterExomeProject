f913c072835e1c63d95d65eec01093afbdc78284
Annabel Beichman <annabel.beichman@gmail.com>
2018-10-19 15:35:17 -0700

    changed output file name for dadi scripts

---
+++ scripts/scripts_20180521/analyses/generate_sfs/vcf2dadi.R
@@ -0,0 +1,23 @@
+install.packages("devtools")
+library(devtools)
+install_github("thierrygosselin/radiator")
+library("optparse")
+library("radiator")
+option_list = list(
+  make_option(c("-vcf", "--vcf"), type="character", default=NULL, 
+              help="path to vcf file", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="out.txt", 
+              help="output file name [default= %default]", metavar="character"),
+  make_option(c("-pop", "--pop"), type="character", default=NULL, 
+              help="population identifier", metavar="character")
+); 
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+data=opt$vcf
+pop=opt$vcf
+
+vcf2dadi(data)
+
+########## UNFINISHED #############
