9545b8eae739faeefa5d751bc8116182f28d3f06
Annabel Beichman <annabel.beichman@gmail.com>
2018-10-26 13:16:58 -0700

    made scripts to convert any vcf to gds format using a wrapper

---
+++ scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_3_ConvertToGDS.generic.Hoffman.R
@@ -0,0 +1,52 @@
+# R script to use on hoffman
+# module load R
+# R 
+# install R packages
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("gdsfmt")
+#biocLite("SNPRelate")
+# usage:
+# module load R
+# Rscript filtering_Step_4_ConvertToGDS.Hoffman.R [set the calldate and indir and infile manually, or can adjust script to make it a command line option.]
+# can be run in the shell (will take ~ 10 min)
+#load R packages
+require(gdsfmt)
+require(SNPRelate)
+library("optparse")
+option_list = list(
+  make_option(c("-vcf", "--vcf"), type="character", default=NULL, 
+              help="path to gzipped vcf file with extension .vcf.gz", metavar="character"),
+  make_option(c("-o", "--outdir"), type="character", default=NULL, 
+              help="path to outputdir [default= %default]", metavar="character")
+); 
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+outdir=opt$outdir
+vcf.fn=opt$vcf
+vcf.fn.prefix=strsplit(vcf.fn,".vcf.gz") # strip off end 
+outfile=paste(outdir,"/",vcf.fn.prefix,".gds",sep="")
+#calldate=20180806 # date gt's were called in format YYYYMMDD (set this manually)
+#todaysdate=format(Sys.Date(),format="%Y%m%d")
+#SCRATCH="/u/flashscratch/a/ab08028/"
+#indir=paste(SCRATCH,"/captures/vcf_filtering/",calldate,"_filtered/",sep="") # this is where your snp vcf file is and where you will save your gds file
+#infilePREFIX="snp_7_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants" # exclude the .vcf.gz suffix # updated filename 20180820
+#infilePREFIX="snp_8_rmRelativesAdmixed_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants"
+#outdir=paste(indir,"/gdsFormat/",sep="")
+#dir.create(outdir,showWarnings = F)
+#read vcf, and reformat to gds (this works with gzipped vcf file)
+
+#vcf.fn = paste(indir,infilePREFIX,".vcf.gz",sep="")
+
+# took ~ 5 mins (only need to do once -- in future can just open the gds file)
+snpgdsVCF2GDS(vcf.fn, outfile, method="biallelic.only")
+
+#summary -- write it out
+# Open an output file
+sink(file=paste(outfile,".summary.txt",sep=""))
+snpgdsSummary(outfile)
+# close summary file
+sink()
+
+# you can then download the gds file and work with it in R on your home computer: for me download to: /Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/datafiles/snps_gds
---
+++ scripts/scripts_20180521/data_processing/variant_filtering/filtering_Step_3_ConvertToGDS.generic.Hoffman.wrapper.sh
@@ -0,0 +1,20 @@
+############## wrapper for GDS conversion script #############
+
+
+module load R
+git.dir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
+
+script.dir=$git.dir/scripts/scripts_20180521/data_processing/variant_filtering/
+script=filtering_Step_3_ConvertToGDS.generic.Hoffman.R
+
+genotypeDate=20180806
+vcfdir=/u/flashscratch/a/ab08028/captures/vcf_filtering/${genotypeDate}_filtered
+outdir=$vcfdir/gdsFormat
+mkdir -p $outdir
+# list of gzipped vcfs you want to convert (can be any VCFs)
+vcfs2gds='snp_8_rmRelatives_keepAdmixed_passingBespoke_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants.vcf.gz snp_8_rmRelativesAdmixed_maxNoCallFrac_0.2_passingBespoke_passingAllFilters_postMerge_raw_variants.vcf.gz'
+for vcf in $vcfs2gds
+do
+Rscript $script.dir/$script --vcf $vcf --outdir $outdir
+done
+# output file name will be input file name with .gds extension instead of .vcf.gz extension
