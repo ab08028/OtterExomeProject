65aecec9c0e37bae5fcdf36ace261387a925d170
Annabel Beichman <ab08028@n4033.(none)>
2018-10-30 16:37:35 -0700

    updated getting sfs at every stage

--- scripts/scripts_20180521/data_processing/variant_filtering/redoFilteringSteps/filtering_Step_1_g_GetSFSAtEveryStage.partial.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/redoFilteringSteps/filtering_Step_1_g_GetSFSAtEveryStage.partial.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #$ -cwd
-#$ -l h_rt=23:00:00,h_data=16G
+#$ -l h_rt=50:00:00,h_data=16G,highp
 #$ -N makeSFS_eachStage
 #$ -o /u/flashscratch/a/ab08028/captures/reports/GATK
 #$ -e /u/flashscratch/a/ab08028/captures/reports/GATK
