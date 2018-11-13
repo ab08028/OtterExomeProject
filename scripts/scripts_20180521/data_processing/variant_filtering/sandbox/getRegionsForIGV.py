8635b514ad8a290efc7086bb4253296711d20a1b
Annabel Beichman <annabel.beichman@gmail.com>
2018-11-13 14:18:55 -0800

    updated igv sc

--- scripts/scripts_20180521/data_processing/variant_filtering/sandbox/getRegionsForIGV.py
+++ scripts/scripts_20180521/data_processing/variant_filtering/sandbox/getRegionsForIGV.py
@@ -65,7 +65,7 @@ def selectForIGV(inputvcfilename,outfilename,bamdir,igvdir):
         #myAN=myCalled *2 # all called alleles
         # if it's a singleton (assumes no no-calls)
         if myAC==1 or myAC==(len(allCalls)-1):
-            print("found a singleton!"+"AC = " + myAC)
+            print("found a singleton!"+"AC = " + str(myAC))
             samplesCalls=dict(zip(samples,allCalls))
             # gets you name of heterozygous individual(s)
             keys=[key for key,value in samplesCalls.items() if value=="0/1"]
