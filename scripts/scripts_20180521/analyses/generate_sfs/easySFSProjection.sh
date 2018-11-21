2f193a0df4a874289b9f6489ba1c081f527618ac
Annabel Beichman <ab08028@n6200.(none)>
2018-11-21 09:50:09 -0800

    updated a few scripts

--- scripts/scripts_20180521/analyses/generate_sfs/easySFSProjection.sh
+++ scripts/scripts_20180521/analyses/generate_sfs/easySFSProjection.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #$ -cwd
-#$ -l h_rt=30:00:00,h_data=16G,highp
+#$ -l h_rt=30:00:00,h_data=32G,highp
 #$ -N easySFSPreview
 #$ -o /u/flashscratch/a/ab08028/captures/reports/SFS
 #$ -e /u/flashscratch/a/ab08028/captures/reports/SFS
