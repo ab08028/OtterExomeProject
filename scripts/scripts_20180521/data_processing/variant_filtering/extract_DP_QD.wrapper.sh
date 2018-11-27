8077350c8ceecd856c8bdb39cc88d6627d77f0d4
Annabel Beichman <annabel.beichman@gmail.com>
2018-11-27 15:36:21 -0800

    ** made big changes: based on plot of QD values, changing QD filter to < 10 instead of <2 . Also adding variant eval (experimental) and separating out QD filter from GATKHF

--- scripts/scripts_20180521/data_processing/variant_filtering/extract_DP_QD.wrapper.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/extract_DP_QD.wrapper.sh
@@ -1,7 +1,6 @@
 gitdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
 scriptdir=$gitdir/scripts/scripts_20180521/data_processing/variant_filtering
 
-script=extract_DP.py 
 
 genotypedate=20180806
 wd=$SCRATCH/captures/vcf_filtering
@@ -9,6 +8,13 @@ vcfdir=$wd/${genotypedate}_filtered # date you called genotypes
 
 vcf=all_1_TrimAlt_raw_variants.vcf.gz
 scaff=GL896899.1
-output=$vcfdir/${scaff}.${vcf%.vcf.gz}.DP.dist.txt
-python $scriptdir/$script --VCF $vcfdir/$vcf --scaffold $scaff --outfile $output
-gzip $output
+
+# get DP dist:
+
+python $scriptdir/extract_DP.py  --VCF $vcfdir/$vcf --scaffold $scaff --outfile $vcfdir/${scaff}.${vcf%.vcf.gz}.DP.dist.txt
+gzip $vcfdir/${scaff}.${vcf%.vcf.gz}.DP.dist.txt
+
+
+# get QD dist: 
+python $scriptdir/extract_QD.py  --VCF $vcfdir/$vcf --scaffold $scaff --outfile $vcfdir/${scaff}.${vcf%.vcf.gz}.QD.dist.txt
+gzip $vcfdir/${scaff}.${vcf%.vcf.gz}.QD.dist.txt
