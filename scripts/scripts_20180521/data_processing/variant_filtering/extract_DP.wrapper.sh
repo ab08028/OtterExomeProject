556414de2d3d8dffc596f2afbe0ce45c78074346
Annabel Beichman <annabel.beichman@gmail.com>
2018-11-27 09:55:11 -0800

    fixed tab

---
+++ scripts/scripts_20180521/data_processing/variant_filtering/extract_DP.wrapper.sh
@@ -0,0 +1,14 @@
+gitdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
+scriptdir=$gitdir/scripts/scripts_20180521/data_processing/variant_filtering
+
+script=extract_DP.py 
+
+genotypedate=20180806
+wd=$SCRATCH/captures/vcf_filtering
+vcfdir=$wd/${genotypedate}_filtered # date you called genotypes
+
+vcf=all_1_TrimAlt_raw_variants.vcf.gz
+scaff=GL896899.1
+output=$wd/${scaff}.${vcf%.vcf.gz}.DP.dist.txt
+python extract_DP.py --VCF $vcfdir/$vcf --scaffold $scaff --outfile $output
+gzip $output
