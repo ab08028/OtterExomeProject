8cc4330edc09f8ba5098d5342b8fffb1c0901e16
Annabel Beichman <annabel.beichman@gmail.com>
2018-12-12 13:58:54 -0800

    experimenting with het filter

--- scripts/scripts_20180521/analyses/generate_sfs/easySFS_4_carryOutProjection.sh
+++ scripts/scripts_20180521/analyses/generate_sfs/easySFS_4_carryOutProjection.sh
@@ -17,11 +17,12 @@
 source /u/local/Modules/default/init/modules.sh
 module load python/2.7
 bgzip=/u/home/a/ab08028/klohmueldata/annabel_data/bin/tabix-0.2.6/bgzip
-todaysdate=20181127-64gb
-genotypeDate=20180806
-noCallFrac=0.9
-vcfdir=/u/flashscratch/a/ab08028/captures/vcf_filtering/${genotypeDate}_filtered/neutralVCF_allPops
-popFile=/u/flashscratch/a/ab08028/captures/samples/samplesPop.Headers.forEasySFS.2.txt
+#todaysdate=20181212
+todaysdate=`date +%Y%m%d`
+genotypeDate=20181119
+noCallFrac=1.0
+vcfdir=/u/flashscratch/a/ab08028/captures/vcf_filtering/${genotypeDate}_filtered/neutral_and_cds_VCFs/neutralVCFs/
+popFile=/u/flashscratch/a/ab08028/captures/samples/samplesPop.Headers.forEasySFS.3.20181119.txt
 # this has admixed in it , but they aren't in pop file
 #easySFS=/u/home/a/ab08028/klohmueldata/annabel_data/bin/easySFS/easySFS.abContinueMod.py 
 
@@ -32,33 +33,36 @@ scriptdir=${gitdir}/analyses/generate_sfs/
 easySFS=$scriptdir/easySFS.abModified.2.noInteract.Exclude01Sites.20181121.py  # this is my modification
 # this version of script excludes sites that are 0-1 across all populations (maybe) -- not sure if it does yet. 
 
-## choose your projections:
-#projections="20,20,20,20,20"
+## choose your projections: choosing for now: 
+# CA,AK,AL,COM,KUR 
+projections="12,14,16,16,12" # may change these after lab meeting
 ### NOTE: projection values must be in same order as populations are in your popFile (this isn't ideal -- at some point I am going to modify the easySFS script)
 # note that order is CA,AK,AL,COM,KUR 
 
-outdir=/u/flashscratch/a/ab08028/captures/analyses/SFS/$genotypeDate/easySFS
+outdir=/u/flashscratch/a/ab08028/captures/analyses/SFS/$genotypeDate/easySFS/projection-${todaysdate}
 mkdir -p $outdir
 # had to modify easySFS so that it wouldn't prompt a "yes/no" response about samples that are missing from VCF file
+# write projection choices into a readme
 
+echo "CA,AK,AL,COM,KUR : $projections " > $outdir/projectionChoices.${todaysdate}.txt
 # make sure vcf isn't zipped
 
-snpvcf=neutral_snp_8b_forEasySFS_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_passingBespoke_passingAllFilters_postMerge_raw_variants.vcf
-allSitesvcf=neutral_all_8_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz
+snpvcf=neutral.snp_8b_forEasySFS_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_passingBespoke_passingAllFilters_postMerge_raw_variants.vcf
+allSitesvcf=neutral.all_8_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz
 #vcf='neutral.all_8a_rmRelatives_rmAdmixedOutliers_passingBespoke_maxNoCallFrac_'${noCallFrac}'_passingBespoke_passingAllFilters_postMerge_raw_variants.vcf'
 # hoping I can get it to work with this neutral all-sites file so that I have monomorphic sites sampled as well. This would be ideal.
 #gunzip ${vcf}.gz
 
 ### NOTE: projection values must be in same order as populations are in your popFile (this isn't ideal -- at some point I am going to modify the easySFS script)
 # note that order is CA,AK,AL,COM,KUR 
-$easySFS -i $vcfdir/${snpvcf} -p $popFile -a -v --proj $projections -f -o $outdir/projection-${todaysdate}
+$easySFS -i $vcfdir/${snpvcf} -p $popFile -a -v --proj $projections -f -o $outdir
 # test run only had --proj 20 (maybe just projects 1 population? will have to see)
 # -f forces overwrite of outdir
-$bgzip ${vcf}
+# $bgzip ${vcf}
 # then do for SYN and MIS (eventually)
 ########## get counts of monomorphic sites to add to the SFSes ############
-python $scriptdir/getMonomorphicProjectionCounts.py --vcf $vcfdir/${allSitesvcf} --popMap $popFile --proj $projections --popIDs CA,AK,AL,COM,KUR --outdir $outdir/projection-${todaysdate}
-
+python $scriptdir/getMonomorphicProjectionCounts.py --vcf $vcfdir/${allSitesvcf} --popMap $popFile --proj $projections --popIDs CA,AK,AL,COM,KUR --outdir $outdir
+# test: started 12:20
 # need to add these to the 0/0 bin for everything. How to do that? Manually? dadi could do in python when I parse results because it's masked anyway. for fastsimcoal need to add to SFS directly 
 # in the 0/0 bin. (but want to add to what's already there.)
 # maybe a little R or python script? (coudl read in the fsc SFSes and add this to 0/0 bin?)
@@ -71,3 +75,25 @@ python $scriptdir/getMonomorphicProjectionCounts.py --vcf $vcfdir/${allSitesvcf}
 # --popMap /Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/information/samples/easySFSPopMapFiles/samplesPop.Headers.forEasySFS.2.goingtoModifyFor20181119.txt \
 # --proj 14,16,16,16,14 --popIDs CA,AK,AL,COM,KUR \
 # --outdir /Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/troubleshooting/testRunsEasySFSModificatoins
+
+## testing that my monormophic script matches what GATK would give:
+# zcat $vcfdir/$allSitesvcf | grep -v "#" | grep "0/0" | grep -v "0/1" | grep -v "1/1" -c
+# # lines that are all 0/0 and no 0/1 and 1/1 (doesn't account for missing data or projection values yet: 6585427
+# # Okay, so they are on the right order of magnitude. 
+# # how to test that this is right? compare to GATK with nocallfrac and restrict to just one population
+# vcfdir=/u/flashscratch/a/ab08028/captures/vcf_filtering/20181119_filtered/neutral_and_cds_VCFs/neutralVCFs/
+# allSitesvcf=neutral.all_8_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz
+# wd=/u/flashscratch/a/ab08028/sandbox/monomorphicCounts
+# GATK=/u/home/a/ab08028/klohmueldata/annabel_data/bin/GenomeAnalysisTK-3.7/GenomeAnalysisTK.jar
+# REFERENCE=/u/home/a/ab08028/klohmueldata/annabel_data/ferret_genome/Mustela_putorius_furo.MusPutFur1.0.dna.toplevel.fasta
+# java -jar $GATK \
+# -R $REFERENCE \
+# -T SelectVariants \
+# --variant ${vcfdir}/$allSitesvcf \
+# --maxNOCALLnumber 6 \
+# -selectType NO_VARIATION \
+# -se 'Elut_CA' \
+# -o $wd/test.MonomorphicCounter.CA.vcf.gz
+# runs in 15 min
+# # line count (without # lines) should equal 5790572 for CA count.
+ 
---
+++ scripts/scripts_20180521/analyses/generate_sfs/plotSFS/plotFromEasySFS.R
@@ -0,0 +1,79 @@
+require(reshape2)
+require(ggplot2)
+require(RColorBrewer)
+############# Plot FSC format SFS in R ############
+############### set up your colors -- keep this consistent across all plots ######
+require(RColorBrewer)
+colorPal=RColorBrewer::brewer.pal(n=6,name = "Dark2")
+colors=list(CA=colorPal[1],BAJ=colorPal[7],AK=colorPal[2],AL=colorPal[3],COM=colorPal[4],KUR=colorPal[5]) # your population colors
+
+######### file specifics ######
+genotypeDate="20181119"
+dateOfProjection="20181212" # date you did the projection (so you can try different projections)
+data.dir=paste("/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/datafiles/SFS/",genotypeDate,"/easySFS_projection/projection-",dateOfProjection,"/",sep="")
+plot.dir=paste("/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/plots/SFS/",genotypeDate,"/easySFS_projection/",sep="")
+fsc.format.dir=paste(data.dir,"fastsimcoal2/",sep="")
+dadi.format.dir = paste(data.dir,"dadi/",sep="")
+popFile=read.table("/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/information/samples/easySFSPopMapFiles/samplesPop.Headers.forEasySFS.3.20181119.txt",header=F) # used in easySFS; update if used a different one. The order of populations in this script is the order that pops are assigned numbers in easy sfs (that's a bit hacky). So get order of pops from this file for 0/1 (double check manually)
+colnames(popFile) <- c("sample","population")
+popOrder <- as.character(unique(popFile$population)) # this should be CA,AK,AL,COM,KUR  for my project
+
+###################### fold sfs function ############
+foldSFS <- function(sfs){
+  foldedSFS <- data.frame()
+  ss=length(sfs$frequency) - 1 # this is the ss in chromosomes
+  foldedBin=ss/2  # half as many as ss ; do seq from 0 --> foldedBins to include monomorphics
+  # note you have to start seq at 0 to include monomorphic bin 
+  for(i in seq(0,foldedBin)){
+    # if it's not the center point (which doesn't get added together)
+    # see wakeley coalescent eq 1.2
+    if(i==ss-i){
+      foldedSFS <- rbind.data.frame(foldedSFS, cbind.data.frame(frequency=i,count=(sfs[sfs$frequency==i,]$count + sfs[sfs$frequency==(ss-i),]$count)/2)) # if it's add midpoint (foldedLen, including 0 monomorphic bin), add together and divide by two (equivalent of not adding together)
+    }
+    else{
+      foldedSFS <- rbind.data.frame(foldedSFS, cbind.data.frame(frequency=i,count=(sfs[sfs$frequency==i,]$count + sfs[sfs$frequency==(ss-i),]$count))) # if not at mid point, just add together like normal
+    }
+  }
+  return(foldedSFS)
+}
+############ Plot 1D sfses for each population ###############################
+# melt sfs:
+for(pop in popOrder) {
+  input <- list.files(fsc.format.dir,pattern=pop,full.names = T)
+  sfs <- read.table(input,skip = 1,header = T) # skip first line: "1 observation"
+  sfs <- melt(sfs) # melt it to make it longways
+  # get frequency from the d0_0 
+  sfs$frequency <- as.numeric(sapply(strsplit(as.character(sfs$variable),"_"),"[",2))
+  sfs <- sfs[,c("frequency","value")]
+  colnames(sfs) <- c("frequency","count") # so it will work in the foldSFS function
+  sfs_fold <- foldSFS(sfs) # gets rid of the trailing zeroes (was folded already but wanted to elimiante extra zeroes)
+  sfs_exclMono <- sfs_fold[sfs_fold$frequency!=0,]
+  p <- ggplot(sfs_exclMono,aes(x=frequency, y=count,fill=pop))+
+    geom_bar(stat="identity")+
+    theme_bw()+
+    scale_x_continuous(breaks=c(seq(1,length(sfs_exclMono$frequency))))+
+    ggtitle(pop)+
+    scale_fill_manual(values=colors[pop])
+  ggsave(paste(plot.dir,pop,"folded.easySFS.proj.sfs.pdf",sep=""),device="pdf",height=5,width=7)
+}
+
+
+############### 2D SFS ###################
+
+# so pop 0 is position 1 in pop Order (pythonic numbering). 
+
+test2D$pop1 <- as.character(row.names(test2D))
+sfs2D <- melt(test2D,id.vars = "pop1")
+head(sfs2D)
+colnames(sfs2D) <- c("pop1","pop0","count")
+sfs2D$frequency_pop0 <- as.numeric(sapply(strsplit(as.character(sfs2D$pop0),"_"),"[",2))
+sfs2D$frequency_pop1 <- as.numeric(sapply(strsplit(as.character(sfs2D$pop1),"_"),"[",2))
+head(sfs2D)
+ggplot(data = sfs2D, aes(x=frequency_pop0, y=frequency_pop1, fill=log10(count))) + 
+  geom_tile()+
+  scale_fill_gradientn(colours=rainbow(8))+
+  theme_bw()+
+  geom_text(aes(label = round(count,1))) +
+  scale_x_continuous(breaks=c(seq(0,length(sfs2D[sfs2D$frequency_pop1==0,]$frequency_pop0)-1)))+
+  scale_y_continuous(breaks=c(seq(0,length(sfs2D[sfs2D$frequency_pop0==0,]$frequency_pop1)-1)))
+View(sfs2D)
