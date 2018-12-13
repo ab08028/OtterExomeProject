bd5d67ef1bd1e87cd20359d4862e39bb11c3a863
Annabel Beichman <annabel.beichman@gmail.com>
2018-12-12 16:51:48 -0800

    added het count to output

--- scripts/scripts_20180521/analyses/generate_sfs/sandbox.getDP.QD.QUAL.easySFS.abModified.2.noInteract.Exclude01Sites.HetFilterExperiments.20181121.py
+++ scripts/scripts_20180521/analyses/generate_sfs/sandbox.getDP.QD.QUAL.easySFS.abModified.2.noInteract.Exclude01Sites.HetFilterExperiments.20181121.py
@@ -287,7 +287,7 @@ def make_datadict(genotypes, pops, maxHetFilter,dpFile,verbose=False,ploidy=1):
     #hetFailSiteCounter=0
     ## Get genotype counts for each population
     dpFile= open(dpFile, "w")
-    dpFile.write("HETFILTER\tQUAL\tDP\tQD\tcalledGTCount\n") 
+    dpFile.write("HETFILTER\tQUAL\tDP\tQD\thetCount\ncalledGTCount\n") 
     for row in genotypes.iterrows():
         ## iterrows() returns a tuple for some reason
         row = row[1]
@@ -325,7 +325,7 @@ def make_datadict(genotypes, pops, maxHetFilter,dpFile,verbose=False,ploidy=1):
                 passStatus="FAIL_HETFILTER_"+str(maxHetFilter)
                 #hetFailSiteCounter += 1
                 calls[pop] =(0,0) # set it as though it's no-call for that population
-                dpFile.write("\t".join(str(x) for x in (passStatus,myqual, myDP, myQD,called_gts)))
+                dpFile.write("\t".join(str(x) for x in (passStatus,myqual, myDP, myQD,het_count,called_gts)))
                 dpFile.write("\n")
 
             else:
