4e10f2bce1b72b4a1db6e83096f54d51c89f33ca
Annabel Beichman <ab08028@n6069.(none)>
2018-12-13 17:46:48 -0800

    updated script

--- scripts/scripts_20180521/analyses/generate_sfs/getMonomorphicProjectionCounts.py
+++ scripts/scripts_20180521/analyses/generate_sfs/getMonomorphicProjectionCounts.py
@@ -173,6 +173,7 @@ def count_PassingMonomorphicSites(pops,projDict,VCF):
     totalCountDict=dict()
     for population in pops.keys():
         countDict[population]=0
+	totalCountDict[population]=0
     for line0 in inVCF:
         if line0.startswith('#'):
             continue
