96df205c76594a9420d5f7b826ef908a7e1eb5be
Annabel Beichman <annabel.beichman@gmail.com>
2019-01-25 13:44:20 -0800

    named wrapper as array instead

--- scripts/scripts_20180521/analyses/slim/sandbox/slim.2Epoch.submitter.sh
+++ scripts/scripts_20180521/analyses/slim/sandbox/slim.2Epoch.submitter.sh
@@ -4,7 +4,7 @@ scriptdir=$gitdir/analyses/slim/
 # 100 replicates
 for i in {1..100}
 do
-qsub slim.2Epoch.wrapper.sh $i
+qsub slim.2Epoch.array.sh $i
 done
 
 # this will submit 100 jobs, each of which is an array of 60 portions.
