ffac2302592c45a4a7c7b2610e8bb71825966dce
Annabel Beichman <annabel.beichman@gmail.com>
2019-02-13 15:27:31 -0800

    changed name of 1epoch script and updated array script accordingly

--- scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.1Epoch/slim.1D.1Epoch.array.sh
+++ scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.1Epoch/slim.1D.1Epoch.array.sh
@@ -11,6 +11,7 @@
 ######### 2 Epoch script generates 100 x 1kb independent blocks ###########
 # want a total of 6000 blocks. so 60 instances of this script for one replicate.
 model=1D.1Epoch
+slimscript=generic.${model}.100kb.20190213.slim # specific slim script
 rep=$1 # doing one replicate, then will set from command line from submission script
 rundate=$2 # date arrays are submitted; set in submitter so as not to have jobs on different days
 outdir=$SCRATCH/captures/analyses/slim/neutralSimulations/$model/$rundate/replicate_${rep} # set this in submission script 
@@ -24,8 +25,6 @@ slim=/u/home/a/ab08028/klohmueldata/annabel_data/bin/SLiM/slim_build/slim # loca
 ############## files and dirs ############
 gitdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/ # project github location
 scriptdir=$gitdir/scripts/scripts_20180521/analyses/slim/neutralSimulations/$model # location of slim scripts
-slimscript=generic.1D.2Epoch.100kb.10genContraction.20180125.slim # specific slim script
-
 
 ######## parameters #############
 todaysdate=`date +%Y%m%d`
