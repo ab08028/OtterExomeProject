19b519cd3913e33ec0bda6ca67812fca09e95a11
Annabel Beichman <ab08028@login1.(none)>
2019-02-27 14:32:12 -0800

    fixed submitter script to do reps 1..11

--- scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.2Epoch.30gen/slim.1D.2Epoch.30gen.submitter.sh
+++ scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.2Epoch.30gen/slim.1D.2Epoch.30gen.submitter.sh
@@ -2,12 +2,12 @@ model=1D.2Epoch.30gen
 gitdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
 scriptdir=$gitdir/scripts/scripts_20180521/analyses/slim/neutralSimulations/${model}
 todaysdate=`date +%Y%m%d` # don't want to use todays date because then different time starting arrays could get messed up
-# 100 replicates; starting with 10
-for i in 1 10
+# 100 replicates; starting with 11
+for i in {1..11}
 do
 # name job slimRepX
 qsub -N slimRep$i $scriptdir/slim.${model}.array.sh $i $todaysdate
 done
 
-# this will submit 100 jobs, each of which is an array of 60 portions.
+# this will submit 11 jobs, each of which is an array of 60 portions.
 
