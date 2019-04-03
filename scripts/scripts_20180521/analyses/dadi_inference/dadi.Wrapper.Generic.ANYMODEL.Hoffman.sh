6a630a936140550efc8b455696536b8ebf37c58f
Annabel Beichman <ab08028@login4.(none)>
2019-04-03 11:23:51 -0700

    new models

--- scripts/scripts_20180521/analyses/dadi_inference/dadi.Wrapper.Generic.ANYMODEL.Hoffman.sh
+++ scripts/scripts_20180521/analyses/dadi_inference/dadi.Wrapper.Generic.ANYMODEL.Hoffman.sh
@@ -40,10 +40,13 @@ sfssuffix=plusMonomorphic.sfs
 ### want to make a slightly fancier outdir that is the model / date or something like that eventually. 
 # run multiple models for multiple popuations?
 #scripts='1D.1Bottleneck.dadi.py 1D.2Bottleneck.dadi.py 1D.2Epoch.dadi.py' # list of models you want to run
-scripts='1D.2Epoch.dadi.py 1D.1Bottleneck.TB20gen.dadi.py'
+#scripts='1D.2Epoch.dadi.py 1D.1Bottleneck.TB20gen.dadi.py'
+# want to run with new starting parameters
+scripts='1D.2Epoch.newStartValues.dadi.py 1D.2Epoch.LargeFoldChange.dadi.py'
 #scripts='1D.1Epoch.dadi.py' # just this one for now
 #for pop in CA AK AL COM KUR
-for pop in MED BER
+for pop in CA AK AL COM KUR 
+#MED BER
 do
 # get total sites from total sites file that was written out as part of my easySFS scripts
 L=`grep $pop $sfsdir/$pop-[0-9]*.totalSiteCount.L.withMonomorphic.txt | awk '{print $2}'`
