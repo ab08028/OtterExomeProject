4e77e01abdb82a10fde0200c8582432adabfe3ff
Annabel Beichman <ab08028@n7425.(none)>
2019-04-05 10:48:54 -0700

    updtead kur

--- scripts/scripts_20180521/analyses/slim/neutralSimulations/populationSpecificModels/1D.2Epoch.KUR/slim.1D.2Epoch.KUR.array.sh
+++ scripts/scripts_20180521/analyses/slim/neutralSimulations/populationSpecificModels/1D.2Epoch.KUR/slim.1D.2Epoch.KUR.array.sh
@@ -10,7 +10,8 @@
 
 ######### 2 Epoch script generates 100 x 1kb independent blocks ###########
 # want a total of 6000 blocks. so 60 instances of this script for one replicate.
-model=1D.2Epoch.KUR
+pop=KUR
+model=1D.2Epoch.${pop}
 rep=$1 # doing one replicate, then will set from command line from submission script
 rundate=$2 # date arrays are submitted; set in submitter so as not to have jobs on different days
 outdir=$SCRATCH/captures/analyses/slim/neutralSimulations/$model/$rundate/replicate_${rep} # set this in submission script 
@@ -24,7 +25,7 @@ slim=/u/home/a/ab08028/klohmueldata/annabel_data/bin/SLiM/slim_build/slim # loca
 ############## files and dirs ############
 gitdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/ # project github location
 scriptdir=$gitdir/scripts/scripts_20180521/analyses/slim/neutralSimulations/populationSpecificModels/$model # location of slim scripts
-slimscript=generic.1D.2Epoch.100kb.CA.slim # specific slim script
+slimscript=generic.1D.2Epoch.100kb.${pop}.slim # specific slim script
 
 
 ######## parameters #############
