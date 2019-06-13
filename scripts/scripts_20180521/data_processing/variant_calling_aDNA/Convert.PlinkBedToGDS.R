1f316fd719edb8347305baa8fc37d82b8e264dde
Annabel Beichman <annabel.beichman@gmail.com>
2019-06-13 14:31:24 -0700

    updating covnersions

--- scripts/scripts_20180521/data_processing/variant_calling_aDNA/Convert.PlinkBedToGDS.R
+++ scripts/scripts_20180521/data_processing/variant_calling_aDNA/Convert.PlinkBedToGDS.R
@@ -13,10 +13,10 @@
 #load R packages
 require(gdsfmt)
 require(SNPRelate)
-library("optparse")
+require("optparse")
 option_list = list(
-  make_option(c("-PlinkBed", "--PlinkBed"), type="character", default=NULL, 
-              help="path to bed file from plink", metavar="character"),
+  make_option(c("-PlinkPrefixPath", "--PlinkPrefixPath"), type="character", default=NULL, 
+              help="path to plink files and their prefix (e.g. /path/to/files/plinkoutputprefix) which contains .fam, .bed and .bim", metavar="character"),
   make_option(c("-o", "--outdir"), type="character", default=NULL, 
               help="path to outputdir [default= %default]", metavar="character")
 ); 
@@ -24,13 +24,16 @@ opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
 outdir=opt$outdir
-bed.fn=opt$PlinkBed
+plink=opt$PlinkBed
 # note that basename will get you just the file name from a path ; dirname will get you just the dir
-bed.fn.prefix=strsplit(basename(bed.fn),".bed") # strip off end 
+#bed.fn.prefix=strsplit(basename(bed.fn),".bed") # strip off end 
 outfile=paste(outdir,"/",bed.fn.prefix,".gds",sep="")
+bed.fn=paste(plink,".bed",sep="")
+fam.fn=paste(plink,".fam",sep="")
+bim.fn=paste(plink,".bim",sep="")
 
 # took ~ 5 mins (only need to do once -- in future can just open the gds file)
-snpgdsVCF2GDS(bed.fn, outfile, method="biallelic.only")
+snpgdsBED2GDS(bed.fn = bed.fn, fam.fn=fam.fn, bim.fn = bim.fn,outfile, method="biallelic.only")
 
 #summary -- write it out
 # Open an output file
