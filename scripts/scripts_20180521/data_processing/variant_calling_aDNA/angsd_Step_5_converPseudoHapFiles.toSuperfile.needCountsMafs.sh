809d2de69f32dd176e9d64762fe2562abd1053ba
Annabel Beichman <annabel.beichman@gmail.com>
2019-06-24 16:10:41 -0700

    updated low cov

--- scripts/scripts_20180521/data_processing/variant_calling_aDNA/angsd_Step_5_converPseudoHapFiles.toSuperfile.needCountsMafs.sh
+++ scripts/scripts_20180521/data_processing/variant_calling_aDNA/angsd_Step_5_converPseudoHapFiles.toSuperfile.needCountsMafs.sh
@@ -22,12 +22,13 @@ ref=mfur # only work with MFUR when going into VEP!
 # have corresponding maf and count files that have exact same filters (eventually want to make a script that makes these at the same time)
 # these have EXACT same filters and same # of sites -- eventually want to combine the forming of these files so they can't be offset
 ############################################### high coverage ################################################
+###### these were hand checked that they have the same sites contained; if diff filters had been used these wouldn't be safe to combine
+# but it's okay because I check # of sites in this script, and I check that the marker IDs are the same using my python parsing script
+# so if you accidentally concat things that shouldn't, it should be caught (but don't ;) ) 
 hapDate="20190612-highcov-pseudoHaps"
 mafCountDate="20190524-highcov-AFprior"
 basename=angsdOut.mappedTo${ref}
 
-#for angsdDate in $dates
-#do
 indir=$hapDir/$hapDate # location of your posterior probs
 GLDir=$wd/angsd-GLs/$mafCountDate
 outdir=$wd/VEP/pseduoHaps/$hapDate
@@ -63,13 +64,13 @@ paste <(zcat $GLDir/$mafs) <(zcat $indir/$hapFile) <(zcat $GLDir/$counts) | grep
 gzip -f $indir/${hapoutput}
 
 ############################################### low coverage ################################################
-
+###### these were hand checked that they have the same sites contained; if diff filters had been used these wouldn't be safe to combine
+# but it's okay because I check # of sites in this script, and I check that the marker IDs are the same using my python parsing script
+# so if you accidentally concat things that shouldn't, it should be caught (but don't ;) ) 
 hapDate="20190612-lowcov-pseudoHaps"
 mafCountDate="20190524-lowcov-AFprior"
 basename=angsdOut.mappedTo${ref}
 
-#for angsdDate in $dates
-#do
 indir=$hapDir/$hapDate # location of your posterior probs
 GLDir=$wd/angsd-GLs/$mafCountDate
 outdir=$wd/VEP/pseduoHaps/$hapDate
@@ -78,15 +79,15 @@ mkdir -p $outdir
 # I am using GPs, but again, could be either:
 #cdsSuperfile=${basename}.superfile.GPs.mafs.counts.cdsOnly.0based.bed.gz
 hapFile=${basename}.haplo.gz
-hapoutput=${hapFile%.haplo.gz}.superfile.0based.bed
+hapoutput=${hapFile%.haplo.gz}.pseudoHaps.superfile.0based.bed
 hapVEPOutput=${hapFile%.haplo.gz}.1based.VEP.input.txt
 mafs=${basename}.mafs.gz
 counts=${basename}.counts.gz
-hapSites=`zcat $hapDate/$hapFile | wc -l `
+hapSites=`zcat $indir/$hapFile | wc -l`
 mafSites=`zcat $GLDir/$mafs | wc -l`
 if [ $hapSites -eq $mafSites ]
 then
-echo "they have the same number of sites!"
+echo "they have the same number of sites"
 else 
 echo "watch out -- differing number of sites"
 fi
@@ -97,7 +98,7 @@ angsdheaders=`paste <(zcat $GLDir/$mafs | head -n1) <(zcat $indir/$hapFile | hea
 # so want to add a bed header too, which will be 
 bedhead="#chrom\tstart0based\tend\tmarkerID\tempty5\tempty6\tempty7\tempty8\tempty9\tempty10\tempty11\tempty12"
 
-comboheader=`echo -e "$bedhead\t$hapHeader"` # need the "" and -e to get the tabs in
+comboheader=`echo -e "$bedhead\t$angsdheaders"` # need the "" and -e to get the tabs in
 echo -e "$comboheader" >  $indir/${hapoutput} 
 
 
