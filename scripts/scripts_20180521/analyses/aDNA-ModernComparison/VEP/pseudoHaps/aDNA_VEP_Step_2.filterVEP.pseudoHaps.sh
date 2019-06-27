ca199f8c5fa0aba6b849b8cce11baf275e189267
Annabel Beichman <ab08028@login4.(none)>
2019-06-27 11:37:21 -0700

    filter pseudohap vep results

---
+++ scripts/scripts_20180521/analyses/aDNA-ModernComparison/VEP/pseudoHaps/aDNA_VEP_Step_2.filterVEP.pseudoHaps.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#$ -l h_rt=24:00:00,h_data=3G
+#$ -N filter_vep
+#$ -cwd
+#$ -m bea
+#$ -o /u/flashscratch/a/ab08028/captures/reports/angsd
+#$ -e /u/flashscratch/a/ab08028/captures/reports/angsd
+#$ -M ab08028
+
+# Filter VEP output to separate missense, synonymous and LOF mutations that are in CANONICAL transcripts
+# Then want to turn into coordinates 
+
+
+# load your modules:
+source /u/local/Modules/default/init/modules.sh
+module load perl/5.10.1
+module load htslib
+vepdir=/u/home/a/ab08028/klohmueldata/annabel_data/bin/ensembl-vep/
+
+# directories: 
+gitDir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
+scriptDir=$gitDir/scripts/scripts_20180521/
+
+SCRATCH=/u/flashscratch/a/ab08028
+wd=$SCRATCH/captures/aDNA-ModernComparison
+#GLdir=$wd/angsd-GLs
+
+ref=mfur # only work with MFUR when going into VEP!
+dates="20190524-highcov-AFprior 20190524-lowcov-AFprior 20190524-highcov-UNIFprior 20190524-lowcov-UNIFprior" # set of angsdDates you want to process 
+basename=angsdOut.mappedTo${ref}
+for angsdDate in $dates
+do
+indir=$wd/VEP/$angsdDate 
+input=${basename}.superfile.GPs.mafs.counts.cdsOnly.1based.VEPInput.VEP.output.pick.tbl.gz
+
+# 20190611: adding --pick to the initial run of VEP so that it only picks one annotation per variant
+# this is what I thought filter-vep was doing, but on rare occasions it was still outputting a couple annotations per variant; don't want that. So for now, I'm just picking based on the VEP criteria (severity and validtity hierarchy). This doesn't change anything about this filtering script except the name of the input file. 
+
+############## Filter Synonymous/NS ##########################
+###### NS (missense):
+$vepdir/filter_vep --filter "Consequence is missense_variant and CANONICAL is YES" \
+--input_file $indir/$input \
+--output_file $indir/${input%.tbl.gz}.missense.tbl \
+--force_overwrite
+
+###### LOF (stop_gained):
+$vepdir/filter_vep --filter "Consequence is stop_gained and CANONICAL is YES" \
+--input_file $indir/$input \
+--output_file $indir/${input%.tbl.gz}.stopgained.tbl \
+--force_overwrite
+
+###### S (synonymous):
+$vepdir/filter_vep --filter "Consequence is synonymous_variant and CANONICAL is YES" \
+--input_file $indir/$input \
+--output_file $indir/${input%.tbl.gz}.synonymous.tbl \
+--force_overwrite
+
+done
+
---
+++ scripts/scripts_20180521/analyses/aDNA-ModernComparison/VEP/pseudoHaps/aDNA_VEP_Step_3.vepOutputBackToBed.pseudoHaps.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#$ -l h_rt=24:00:00,h_data=3G
+#$ -N VEP2BED
+#$ -cwd
+#$ -m bea
+#$ -o /u/flashscratch/a/ab08028/captures/reports/angsd
+#$ -e /u/flashscratch/a/ab08028/captures/reports/angsd
+#$ -M ab08028
+
+# Filter VEP output to separate missense, synonymous and LOF mutations that are in CANONICAL transcripts
+# Then want to turn into coordinates 
+source /u/local/Modules/default/init/modules.sh
+module load bedtools
+# directories: 
+gitDir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/
+scriptDir=$gitDir/scripts/scripts_20180521/
+
+SCRATCH=/u/flashscratch/a/ab08028
+wd=$SCRATCH/captures/aDNA-ModernComparison
+
+ref=mfur # only work with MFUR when going into VEP!
+dates="20190524-highcov-AFprior 20190524-lowcov-AFprior"
+categories="synonymous missense stopgained"
+
+#  20190524-highcov-UNIFprior 20190524-lowcov-UNIFprior" # set of angsdDates you want to process 
+basename=angsdOut.mappedTo${ref}
+for angsdDate in $dates
+do
+indir=$wd/VEP/$angsdDate 
+GLdir=$wd/angsd-GLs/$angsdDate 
+mkdir -p $GLdir/cdsPerCategoryFromVEP
+### convert vep output back to bed coords
+# note vep is 1based, bed is 0based
+
+for angsdDate in $dates
+do
+for category in $categories
+do
+# doesn't matter whether this came from GPs or GLs, it's all about coordinates
+input=$indir/${basename}.superfile.GPs.mafs.counts.cdsOnly.1based.VEPInput.VEP.output.pick.${category}.tbl
+grep -v "#" $input | awk '{OFS="\t";split($2,pos,":");print pos[1],pos[2]-1,pos[2],$1}' > ${input%.tbl}.0based.coordsOnly.bed
+
+#### then intersect with my superfiles #########
+types="GLs GPs" # could do GPs or GLs from these coordinates.
+for type in $types
+do
+# redo this with the -header flag so I get headers
+bedtools intersect -a $GLdir/${basename}.superfile.${type}.mafs.counts.0based.bed.gz -b ${input%.tbl}.0based.coordsOnly.bed -wa -header > $GLdir/cdsPerCategoryFromVEP/${basename}.superfile.${type}.mafs.counts.0based.${category}.bed
+done
+done
+done
+done
+
