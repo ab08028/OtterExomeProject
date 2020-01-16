5628858ce240ef458083b4d6ac27ce827165cdf8
Annabel Beichman <annabel.beichman@gmail.com>
2020-01-16 10:03:19 -0800

    don't write out date

--- scripts/scripts_20180521/analyses/StitchPSMC_SFS_Together/simulateForMSMC/Step_3_plotMSMCResults.Models.Simulations.R
+++ scripts/scripts_20180521/analyses/StitchPSMC_SFS_Together/simulateForMSMC/Step_3_plotMSMCResults.Models.Simulations.R
@@ -6,6 +6,9 @@ require(scales)
 mu=8.64e-09
 model1="dadiModel1.OldShallowContraction"
 model2="dadiModel2.RecentExtremeContraction"
+model3="dadiModel3.BothContractions"
+data.dir="/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/analysisResults/StitchPSMC_SFS_Together/CA/runMSMCOnSimluations/"
+
 ########## function to scale results #######
 # input is .final.txt file, read into a df
 ###### info from msmc guide ons caling:
@@ -26,29 +29,26 @@ scaleMSMC <- function(input,mu,gen,spp,label,category="main"){
   return(input)
 }
 
-################ read in results #############
-data.dir="/Users/annabelbeichman/Documents/UCLA/Otters/OtterExomeProject/results/analysisResults/StitchPSMC_SFS_Together/CA/runMSMCOnSimluations/"
-#models=c("dadiModel1.OldShallowContraction",  "dadiModel2.RecentExtremeContraction")
-#replicate=1
+
 ############################### model 1 #############################
 # this is the actual demographic model
 model=model1
-model1Rect1=data.frame(xmin=0,xmax=1000,ymin=0,ymax=2000)
-model1Rect2=data.frame(xmin=1000,xmax=30000,ymin=0,ymax=4500)
-model1_allReps <- data.frame()
+modelRect1=data.frame(xmin=0,xmax=1000,ymin=0,ymax=2000)
+modelRect2=data.frame(xmin=1000,xmax=30000,ymin=0,ymax=4500)
+model_allReps <- data.frame()
 for(replicate in c(seq(1,10))){
   # check if file is empty; skip if it is
   if(file.exists(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""))){
     results <- read.table(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""),header=T)
     results_scaled <- scaleMSMC(results,mu,gen=7,model,paste("rep_",replicate,sep=""),category="main")
 # note now this result is labeled with its rep number (in the function), so can tell apart
-    model1_allReps = rbind(model1_allReps,results_scaled)
+    model_allReps = rbind(model_allReps,results_scaled)
   }
   else{ print(paste("rep",replicate," does not exist!"))}
 }
-p1 <- ggplot(model1_allReps)+
-  geom_rect(data=model1Rect1,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
-  geom_rect(data=model1Rect2,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+p1 <- ggplot(model_allReps)+
+  geom_rect(data=modelRect1,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+  geom_rect(data=modelRect2,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
   geom_step(stat="identity",size=0.5,aes(x=Left_generations,y=Ne,group=label),color="black")+
   theme_bw()+
   theme(legend.title = element_blank())+
@@ -62,22 +62,22 @@ p1
 ggsave(paste(data.dir,model,"/MSMC.RunOnSimulations.",model,".pdf",sep=""),p1,height=5,width=7)
 ############################### model 2 #############################
 model=model2
-model2Rect1=data.frame(xmin=0,xmax=35,ymin=0,ymax=195)
-model2Rect2=data.frame(xmin=35,xmax=30000,ymin=0,ymax=3500)
-model2_allReps <- data.frame()
+modelRect1=data.frame(xmin=0,xmax=35,ymin=0,ymax=195)
+modelRect2=data.frame(xmin=35,xmax=30000,ymin=0,ymax=3500)
+model_allReps <- data.frame()
 for(replicate in c(seq(1,10))){
   # check if file is empty; skip if it is
   if(file.exists(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""))){
     results <- read.table(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""),header=T)
     results_scaled <- scaleMSMC(results,mu,gen=7,model,paste("rep_",replicate,sep=""),category="main")
     # note now this result is labeled with its rep number (in the function), so can tell apart
-    model2_allReps = rbind(model2_allReps,results_scaled)
+    model_allReps = rbind(model_allReps,results_scaled)
   }
   else{ print(paste("rep",replicate," does not exist!"))}
 }
-p2 <- ggplot(model2_allReps)+
-  geom_rect(data=model2Rect1,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
-  geom_rect(data=model2Rect2,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+p2 <- ggplot(model_allReps)+
+  geom_rect(data=modelRect1,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+  geom_rect(data=modelRect2,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
   geom_step(stat="identity",size=0.5,aes(x=Left_generations,y=Ne,group=label),color="black")+
   theme_bw()+
   theme(legend.title = element_blank())+
@@ -91,6 +91,39 @@ p2
 ggsave(paste(data.dir,model,"/MSMC.RunOnSimulations.",model,".pdf",sep=""),p2,height=5,width=7)
 
 
+
+############################### model 3 #############################
+model=model3
+modelRect1=data.frame(xmin=0,xmax=22,ymin=0,ymax=270)
+modelRect2=data.frame(xmin=22,xmax=1000,ymin=0,ymax=2000)
+modelRect3=data.frame(xmin=1000,xmax=30000,ymin=0,ymax=4500)
+
+model_allReps <- data.frame()
+for(replicate in c(seq(1,10))){
+  # check if file is empty; skip if it is
+  if(file.exists(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""))){
+    results <- read.table(paste(data.dir,model,"/rep_",replicate,"/msmc.RunOn.",model,".sims.out.final.txt",sep=""),header=T)
+    results_scaled <- scaleMSMC(results,mu,gen=7,model,paste("rep_",replicate,sep=""),category="main")
+    # note now this result is labeled with its rep number (in the function), so can tell apart
+    model_allReps = rbind(model_allReps,results_scaled)
+  }
+  else{ print(paste("rep",replicate," does not exist!"))}
+}
+p3 <- ggplot(model_allReps)+
+  geom_rect(data=modelRect1,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+  geom_rect(data=modelRect2,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+  geom_rect(data=modelRect3,aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),fill="purple",alpha=0.4)+
+  geom_step(stat="identity",size=0.5,aes(x=Left_generations,y=Ne,group=label),color="black")+
+  theme_bw()+
+  theme(legend.title = element_blank())+
+  ggtitle(model)+
+  xlab("Generations")+
+  ylab("IICR") +
+  scale_y_log10()+
+  scale_x_log10() 
+
+p3
+ggsave(paste(data.dir,model,"/MSMC.RunOnSimulations.",model,".pdf",sep=""),p3,height=5,width=7)
 
 
 
