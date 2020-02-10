63622a40d2487ee241756dcb4adcd53436e6e636
Annabel Beichman <annabel.beichman@gmail.com>
2020-02-10 10:11:23 -0800

    trying with differnt starting mu parameters

--- scripts/scripts_20180521/analyses/dadi_inference/dadi.Wrapper.2D.CA-AK.sh
+++ scripts/scripts_20180521/analyses/dadi_inference/dadi.Wrapper.2D.CA-AK.sh
@@ -30,24 +30,15 @@ mu=8.64411385098638e-09
 genotypeDate=20181119 # newer gts
 sfsDate=20181221 # projection with 0.75 het filter and these projection values
 hetFilter=0.75
-todaysdate=`date +%Y%m%d`
+todaysdate=20200207
 captures=$SCRATCH/captures/
 sfsdir=$captures/analyses/SFS/$genotypeDate/easySFS/neutral/projection-${sfsDate}-hetFilter-${hetFilter}/dadi-plusMonomorphic/
 dadidir=$captures/analyses/dadi_inference/
 sfssuffix=plusMonomorphic.sfs
-### Make sure this is the correct file #####
-#totalNeut=$captures/vcf_filtering/${genotypeDate}_filtered/bedCoords/neutralCallableSites_perPop/summary.neutralCallableSites.perPop.txt # file with total neutral sites counts for each population 
-### want to make a slightly fancier outdir that is the model / date or something like that eventually. 
-# run multiple models for multiple popuations?
-#scripts='1D.1Bottleneck.dadi.py 1D.2Bottleneck.dadi.py 1D.2Epoch.dadi.py' # list of models you want to run
-#scripts='1D.2Epoch.dadi.py 1D.1Bottleneck.TB20gen.dadi.py'
-# want to run with new starting parameters
-#scripts='1D.2Epoch.newStartValues.dadi.py 1D.2Epoch.LargeFoldChange.dadi.py'
-#scripts='1D.1Epoch.dadi.py' # just this one for now
-#for pop in CA AK AL COM KUR
-#scripts='1D.1Epoch.dadi.py' # just this one for now
-scripts='2D.Bottleneck.Migration.dadi.py 2D.ConstantSize.Migration.dadi.py 2D.ConstantSize.noMigration.dadi.py'
-for popPair in CA-AK CA-KUR AK-KUR AK-AL AL-KUR AK-COM AL-COM CA-AL CA-COM CA-KUR # 
+
+#scripts='2D.Bottleneck.Migration.dadi.py 2D.ConstantSize.Migration.dadi.py 2D.ConstantSize.noMigration.dadi.py'
+scripts='2D.Bottleneck.Migration.dadi.py 2D.ConstantSize.Migration.dadi.py'
+for popPair in CA-AK AK-KUR AK-AL AL-KUR AK-COM AL-COM CA-AL CA-COM CA-KUR # where is com-kur? missing from globus? find it later
 do
 echo $popPair
 # get total sites from total sites file that was written out as part of my easySFS scripts
@@ -58,7 +49,7 @@ do
 
 model=${script%.dadi.py}
 echo "starting inference for $popPair for model $model"
-outdir=$dadidir/$genotypeDate/$popPair/inference_$todaysdate/$model/
+outdir=$dadidir/$genotypeDate/2D_Models/$popPair/inference_$todaysdate/$model/
 mkdir -p $outdir
 # carry out inference with 50 replicates that start with different p0 perturbed params:
 for i in {1..50}
@@ -70,6 +61,7 @@ done
 
 
 echo "concatenating results"
+#rename 2D.ConstantSize.Migration.WBottleneck 2D.Bottleneck.Migration $outdir/*
 grep rundate -m1 $outdir/${popPair}.dadi.inference.${model}.runNum.1.output > $outdir/${popPair}.dadi.inference.${model}.all.output.concatted.txt
 for i in {1..50}
 do
