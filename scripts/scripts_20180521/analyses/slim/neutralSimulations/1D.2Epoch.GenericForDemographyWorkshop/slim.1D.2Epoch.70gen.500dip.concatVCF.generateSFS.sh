86d98eadb69ac31f6a0a1e5f1dbb149d6e0c2007
Annabel Beichman <ab08028@login3.(none)>
2020-03-24 11:59:51 -0700

    updated date

--- scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.2Epoch.GenericForDemographyWorkshop/slim.1D.2Epoch.70gen.500dip.concatVCF.generateSFS.sh
+++ scripts/scripts_20180521/analyses/slim/neutralSimulations/1D.2Epoch.GenericForDemographyWorkshop/slim.1D.2Epoch.70gen.500dip.concatVCF.generateSFS.sh
@@ -1,7 +1,7 @@
 
 ######## concatenate VCF files per replicate, and add a 'chromosome' identifier (that's really a portion identifier)
 ########### set up dir structure ##########
-rundate=20190313 # date of running slim
+rundate=20200324 # date of running slim
 model=1D.2Epoch.GenericForDemographyWorkshop
 wd=/u/flashscratch/a/ab08028/captures/analyses/slim/neutralSimulations/$model/$rundate/ # eventually loop over all replicates 
 sfsdir=$wd/allSFSes
