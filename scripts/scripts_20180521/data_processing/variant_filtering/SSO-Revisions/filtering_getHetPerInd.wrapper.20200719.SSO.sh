502f1b9e8401a2800e76d71bd0ae38bc7940473d
Annabel Beichman <ab08028@login3.(none)>
2020-07-28 11:42:36 -0700

    neutral paths

--- scripts/scripts_20180521/data_processing/variant_filtering/SSO-Revisions/filtering_getHetPerInd.wrapper.20200719.SSO.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/SSO-Revisions/filtering_getHetPerInd.wrapper.20200719.SSO.sh
@@ -15,15 +15,17 @@ wd=$SCRATCH/captures/vcf_filtering
 scriptdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/scripts/scripts_20180521/data_processing/variant_filtering
 script=filtering_getHetPerInd.py
 rundate=20200719_${REFSHORTCODE}
-vcfdir=$wd/${rundate}_filtered
-
+# for all sites:
+#vcfdir=$wd/${rundate}_filtered
+# for neutral:
+vcfdir=$wd/${rundate}_filtered/neutral/neutral_and_cds_VCFs/neutralVCFs
 outdir=$vcfdir/hetPerIndividual/$REFSHORTCODE
 mkdir -p $outdir
 
 # there are a two vcfs I want to look at and compare
 #vcfs='all_7_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
 #vcfs='all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
-vcfs='neutral_and_cds_VCFs/neutralVCFs/neutral.all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
+vcfs='neutral.all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
 for vcf in $vcfs
 do
 python $scriptdir/$script $vcfdir/$vcf $outdir/${vcf}.perIndHet.${REFSHORTCODE}.txt
--- scripts/scripts_20180521/data_processing/variant_filtering/filtering_getHetPerInd.wrapper.MFUR.20181119.sh
+++ scripts/scripts_20180521/data_processing/variant_filtering/filtering_getHetPerInd.wrapper.MFUR.20181119.sh
@@ -16,13 +16,19 @@ scriptdir=/u/home/a/ab08028/klohmueldata/annabel_data/OtterExomeProject/scripts/
 script=filtering_getHetPerInd.py
 rundate=20181119 # date genotypes were called 
 #vcfdir=$wd/${rundate}_filtered
-vcfdir=/u/home/a/ab08028/klohmueldata/annabel_data/captures/vcf_filtering/${rundate}_filtered
+# for all sites:
+#vcfdir=/u/home/a/ab08028/klohmueldata/annabel_data/captures/vcf_filtering/${rundate}_filtered
+# for neutral:
+vcfdir=/u/home/a/ab08028/klohmueldata/annabel_data/captures/vcf_filtering/${rundate}_filtered/neutral_and_cds_VCFs/neutralVCFs/
 
 outdir=$vcfdir/hetPerIndividual/$REFSHORTCODE
 mkdir -p $outdir
 
 # there are a two vcfs I want to look at and compare
-vcfs='all_7_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
+#vcfs='all_7_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
+## adding in neutral vcf:
+vcfs='neutral.all_9_maxHetFilter_0.75_rmRelatives_rmAdmixed_passingBespoke_maxNoCallFrac_1.0_rmBadIndividuals_passingFilters_raw_variants.vcf.gz'
+
 for vcf in $vcfs
 do
 python $scriptdir/$script $vcfdir/$vcf $outdir/${vcf}.perIndHet.${REFSHORTCODE}.txt
